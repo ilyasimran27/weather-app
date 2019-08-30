9966bef119428eb4639deb707c88aeed97ca01dc
Muhammad Imran <50030345+ilyasimran27@users.noreply.github.com>
2019-08-30 14:16:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function change(){
+    document.querySelector("section").style=" background-image: url('images/rainy.gif')";
+}
+function change2(){
+    document.querySelector("section").style=" background-image: url('images/bc.jpg')";
+
+
+}
